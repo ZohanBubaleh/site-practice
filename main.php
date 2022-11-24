c04f02a977320757915f244175a7ce208d997f7f
ZohanBubaleh <106962501+ZohanBubaleh@users.noreply.github.com>
2022-11-24 12:33:02 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# product: http://ogp.me/ns/product#' dir="ltr" lang="ru">
+<head>
+<meta charset="UTF-8" />
+<title>Главная страница проекта Зохана по прозвищу Сладкий Буболех</title>
+<base href="https://zohanbuboleh.ru/" />						
+<meta name="description" content="Сладкий Буболех где купить в Ростове-на-Дону газировку лучшее детское щампанское" />
+<meta name="keywords" content="Газированный напиток, Сладкий Буболех, шипучка, запивон, газировка, сладкая вода, детское шампанское"/>
+<meta name="viewport" content="width=device-width, initial-scale=0.1, user-scalable=yes"/>
+<meta name='HandheldFriendly' content='true'/>
+<meta name='MobileOptimized' content='width'/>
+<meta name='apple-mobile-web-app-capable' content='yes'/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="zohanbuboleh.ru"/>
+<meta property="og:title" content="Главная страница проекта Зохана по прозвищу Сладкий Буболех zohanbuboleh.ru"/>
+<meta property="og:url" content="https//zohanbuboleh.ru/"/>
+<meta property="og:description" content="Сладкий Буболех где купить в Ростове-на-Дону газировку лучшее детское щампанское"/>
+<meta property="og:locale" content="ru_RU"/>
+<meta property="og:restrictions:age" content="14+"/>
+<meta property="og:image" content="https://github.com/ZohanBubaleh/site-practice/blob/main/img/Avatar-Glaza.jpg"/>
+<meta name="cypr-verification" content="e9406c61b00e8df9a071a51f9e24de26"/>
+<meta name="cypr-verification" content="43627181651f8019f600ccba2d19f1d9"/>
+<meta name="msvalidate.01" content="E199666794EC9D7740D333182984537A"/>
+<meta name="p:domain_verify" content="16a57f5b512a4490999accf651c1bb76"/>
+<link rel="preconnect" href="//zohanbubolwh.ru/">
+<link rel="preconnect" href="//google-analytics.com/">
+<link rel="preconnect" href="//www.googletagmanager.com/">
+<link rel="dns-prefetch" href="//zohanbubolwh.ru/">
+<link rel="dns-prefetch" href="//google-analytics.com/">
+<link rel="dns-prefetch" href="//www.googletagmanager.com/">
+<link href="https://github.com/ZohanBubaleh/site-practice/blob/main/img/faviconcola.png" rel="icon" />
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="flex-container">
+      <div class="header">     
+                <?php include 'logo.inc.php' ?>         
+                <?php include 'menu.inc.php' ?>	   
+      </div>       
+        <div class="about_me">
+          <h1><?php  echo $p  ?></h1>
+            <div class="data">
+               <div class="myImg">
+                    <?php  echo '<img src="/img/Avatar-Glaza.jpg">'; ?>                    
+               </div>
+                  <div class="fullname">
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>' $fikename. '<br>'; 
+                          echo 'город', ' ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    лет </p>
+                    <p> Мы научились создавать переменные </p>
+                    <p> Изучили простые операции с ними </p>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  include 'knowledge.inc.php'; ?>
+                    <?php   echo $a, ' ', $b, ' ', $c; ?> <br>
+                                       
+                    <?php
+                        $a = 10;
+                        $b = 20;
+                        $c = $a + $b;
+                        echo $c;
+                    ?>   <br>                
+                     <?php
+                        echo $d;
+                    ?> 
+
+            </div>
+
+            <div class="article">
+                <p class="text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Recusandae asperiores ducimus dolore explicabo. Animi est amet quibusdam molestias! 
+                    Minus laudantium sapiente dignissimos possimus natus cumque delectus sed, accusantium totam quia?
+                </p>
+            </div>
+        </div>
+
+            <?php include 'footer.inc.php' ?>
+
+    </div>
+</body>
+</html>
