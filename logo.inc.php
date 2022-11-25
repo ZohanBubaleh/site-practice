ca416bcbdc4d5a1b4bf5a5b1d876a94a65120519
ZohanBubaleh <106962501+ZohanBubaleh@users.noreply.github.com>
2022-11-25 14:04:00 +0300

    Update logo.inc.php

--- logo.inc.php
+++ logo.inc.php
@@ -1,3 +1,3 @@
 <div class="logo"> 
-    <img src="img/Avatar-Glaza.jpg" alt="Зохан Сладкий Бубалех" title="Зохан Сладкий Бубалех"><img src="img/Fizzy-Bubaleh-3.jpg" alt="Зохан Сладкий Бубалех" title="Зохан Сладкий Бубалех">
+    <img src="img/Avatar-Glaza.jpg" alt="Зохан Сладкий Бубалех" title="Зохан Сладкий Бубалех">
 </div>  
