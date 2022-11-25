6e00211e6a2e3317f48c722e6a16920e9add4cc7
ZohanBubaleh <106962501+ZohanBubaleh@users.noreply.github.com>
2022-11-25 14:07:15 +0300

    Update footer.inc.php

--- footer.inc.php
+++ footer.inc.php
@@ -1,5 +1,5 @@
 <div class="footer">
-   <img src="img/Fizzy-Bubaleh-3.jpg" alt="Зохан Сладкий Бубалех" title="Зохан Сладкий Бубалех"><h2>Полноценное изучение PHP, как и JavaScript и MySQL и Perl и Python <br> - дело не простое, но мы, хотя бы начали это делать.</h2>
+   <h2>Полноценное изучение PHP, как и JavaScript и MySQL и Perl и Python <br> - дело не простое, но мы, хотя бы начали это делать.</h2>
 </div>
 <div class="copyright">
     Zohan - Sweet Bubaleh © 2022-2023 All Rights Reserved
