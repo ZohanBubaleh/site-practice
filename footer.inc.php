343a8ff51e6fce3bf58c598b01989f21ab0f278c
ZohanBubaleh <106962501+ZohanBubaleh@users.noreply.github.com>
2022-11-25 12:50:58 +0300

    Update footer.inc.php

--- footer.inc.php
+++ footer.inc.php
@@ -2,5 +2,5 @@
     <h2>Полноценное изучение PHP, как и JavaScript и MySQL и Perl и Python <br> - дело не простое, но мы, хотя бы начали это делать.</h2>
 </div>
 <div class="copyright">
-    Zohan - Sweet Bubaleh’ © 2022-2023 All Rights Reserved
+    Zohan - Sweet Bubaleh © 2022-2023 All Rights Reserved
 </div>
